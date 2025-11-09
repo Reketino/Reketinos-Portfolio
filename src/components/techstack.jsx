63f62a1408e98de608ca0fd42d6d3f42c36d3248
Reketino <bjornevensk8@gmail.com>
2025-11-09 14:35:38 +0100

    la til jsx sider og importerte react icons

---
+++ src/components/techstack.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { FaJs, FaHtml5, FaCss3Alt, FaReact, FaPython, FaGithub } from "react-icons/fa";
+
+ import { SiNextdotjs, SiRender, SiAdobepremierepro, SiAdobephotoshop, SiVercel, SiSupabase  } from "react-icons/si";
+
+ import { VscVscode } from "react-icons/vsc";
+
+
+
+export default function TechStack() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
