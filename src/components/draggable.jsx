bc07820c6328f0eeefc61371b9d634c89cbe769f
Reketino <bjornevensk8@gmail.com>
2025-11-17 11:21:15 +0100

    Draggable komponent,, må finpusses

---
+++ src/components/draggable.jsx
@@ -0,0 +1,61 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+
+
+export default function Draggable({ id, children, startX= 0, startY= 0, }) {
+const ref = useRef(null);
+
+const [pos, setPos] = useState ({ x: startX, y: startY});
+
+useEffect(() => {
+   const saved = localStorage.getItem(`pos_${id}`);
+    if (saved) {
+      setPos(JSON.parse(saved));
+    } else {
+      localStorage.setItem(
+        `pos_${id}`,
+        JSON.stringify({ x: startX, y: startY })
+      );
+    }
+  }, [id, startX, startY]);
+
+function onPointerDown(e) {
+    if (e.button !== 0) return;
+    
+    const startX = e.clientX;
+    const startY = e.clientY;
+
+    const initX = pos.x;
+    const initY = pos.y;
+
+    function move(ev) {
+        const dx = ev.clientX - startX;
+        const dy = ev.clientY - startY;
+
+        const newPos = {
+            x: initX + dx,
+            y: initY + dy,
+        };
+
+        setPos(newPos);
+        localStorage.setItem(`pos_${id}`, JSON.stringify(newPos));
+    }
+function up() {
+    document.removeEventListener("pointermove", move);
+    document.removeEventListener("pointerup", up);
+}
+document.addEventListener("pointermove", move);
+document.addEventListener("pointerup", up);
+}
+
+  return (
+    <main
+    ref={ref}
+    onPointerDown={onPointerDown}
+    className="absolute cursor-pointer active:cursor-grabbing select-none"
+    style={{ left: pos.x, top: pos.y }}
+    >
+        {children}
+    </main>
+  )
+}
