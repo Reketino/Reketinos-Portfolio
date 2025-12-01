8401f4fda41fa3444c7d5589118dccdde0feb52c
Reketino <bjornevensk8@gmail.com>
2025-12-01 11:47:35 +0100

    404 added

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <main>
+      <h1 className='text-center mt-100 text-4xl'>Reketino is on holiday</h1>
+    </main>
+  )
+}
