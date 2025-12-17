f86675ce53aac1f8df4a1ce01a0b8577b349c893
Reketino <bjornevensk8@gmail.com>
2025-12-17 18:40:56 +0100

    adjusted position of AI

--- src/components/BearChat/chatbubble.jsx
+++ src/components/BearChat/chatbubble.jsx
@@ -5,7 +5,7 @@ export default function ChatBubble({ show }) {
   return (
     <main 
     className='
-    absolute -top-12 right-0
+    absolute -top-14 right-0
     bg-neutral-900 text-neutral-100
     text-xs px-3 py-2 rounded-lg
     shadow-lg border border-white/10
