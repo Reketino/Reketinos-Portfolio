621fe005c1353558a8fd6c7e3c226f276f1186b3
Reketino <bjornevensk8@gmail.com>
2026-01-11 11:17:20 +0100

    Chatbear  pushed in front of title on mobile

--- src/components/BearChat/chatbear.jsx
+++ src/components/BearChat/chatbear.jsx
@@ -22,7 +22,7 @@ export default function ChatBear() {
 
   return (
     <>
-      <main className="fixed bottom-24 right-20">
+      <main className="fixed bottom-24 right-20 z-50">
         <ChatBubble show={showBubble && !open} />
 
         <button
