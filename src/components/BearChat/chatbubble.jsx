12dd650b2126eba6cacabdae663a1170ce29b86a
Reketino <bjornevensk8@gmail.com>
2026-01-18 11:47:14 +0100

    Formatting BearAI components

--- src/components/BearChat/chatbubble.jsx
+++ src/components/BearChat/chatbubble.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function ChatBubble({ show }) {
-    if (!show) return null;
+  if (!show) return null;
   return (
-    <main 
-    className='
+    <main
+      className="
     absolute -top-14 right-0
     bg-neutral-900 text-neutral-100
     text-xs px-3 py-2 rounded-lg
     shadow-lg border border-white/10
     whitespace-nowrap
     animate-fade-in
-    '>
-        Hey👋🏻 got any questions? Ask me
-      <section 
-      className='
+    "
+    >
+      Hey👋🏻 got any questions? Ask me
+      <section
+        className="
       absolute -bottom-1.5 right-4
       w-3 h-3 bg-neutral-900
       rotate-45 border-r border-b border-white/10
-      '
+      "
       />
     </main>
   );
