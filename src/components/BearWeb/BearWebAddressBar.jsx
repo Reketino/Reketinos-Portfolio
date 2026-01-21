96dac3758e640213cc0f38636a0f802fcd31419c
Reketino <bjornevensk8@gmail.com>
2026-01-21 20:35:46 +0100

    Feat: Bookmark in BearWeb

--- src/components/BearWeb/BearWebAddressBar.jsx
+++ src/components/BearWeb/BearWebAddressBar.jsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { MdOutlineStarRate } from "react-icons/md";
 
 export default function BearWebAddressBar({ onNavigate, currentUrl }) {
   const [input, setInput] = useState("");
@@ -17,17 +18,32 @@ export default function BearWebAddressBar({ onNavigate, currentUrl }) {
         e.preventDefault();
         onNavigate(input);
       }}
-      className="bw-addr-form"
+      className="bw-addr-form relative w-full"
     >
       <input
         name="SearchBar"
-        className="bw-addr-input"
+        className="bw-addr-input w-full pr-10"
         placeholder="Search or type URL"
         value={input}
         onFocus={() => setIsEditing(true)}
         onBlur={() => setIsEditing(false)}
         onChange={(e) => setInput(e.target.value)}
       />
+
+      <button
+      type="button"
+      title="Bookmark"
+      className="
+      absolute right-5 top-1/2 -translate-y-1/2
+       rounded-full
+      text-neutral-400
+      text-xl
+      hover:bg-neutral-600
+      "
+      onClick={() => console.log("bookmark clicked")}
+      >
+      <MdOutlineStarRate />
+    </button>
     </form>
   );
 }
