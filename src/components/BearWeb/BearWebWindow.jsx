c847db204ef8f92ece30f59e27edb824f0cdbb16
Reketino <bjornevensk8@gmail.com>
2026-06-23 10:10:46 +0200

    feat: setup const tab

--- src/components/BearWeb/BearWebWindow.jsx
+++ src/components/BearWeb/BearWebWindow.jsx
@@ -6,8 +6,14 @@ import BearWebStart from "./BearWebStart";
 export default function BearWebWindow() {
   const HOME_URL = "https://www.google.com/webhp?igu=1";
 
-  const [url, setUrl] = useState("");
-  const [reloadKey, setReloadKey] = useState(0);
+const [tabs, setTabs] = useState([
+  {
+    id: 1,
+    title: "New Tab",
+    url: "",
+    reloadKey: 0,
+  }
+])
 
   const navigate = (value) => {
     if (!value.trim()) return;
