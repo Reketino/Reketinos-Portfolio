9c464275a4bf2e11494809c510b82fbff231f2ce
Reketino <bjornevensk8@gmail.com>
2026-07-02 10:33:35 +0200

    chore: removed useEffect

--- src/components/BearWeb/BearWebWindow.jsx
+++ src/components/BearWeb/BearWebWindow.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import BearWebTopbar from "./BearWebTopbar";
 import BearWebStart from "./BearWebStart";
 import BearWebTabs from "./BearWebTabs";
