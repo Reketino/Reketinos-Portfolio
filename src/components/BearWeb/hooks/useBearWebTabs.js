0feb1d9385ee0a3369ffb0610e64ab056d62a672
Reketino <bjornevensk8@gmail.com>
2026-07-03 09:56:25 +0200

    feat: added const tabs & activeTabId to useBearWebTabs

--- src/components/BearWeb/hooks/useBearWebTabs.js
+++ src/components/BearWeb/hooks/useBearWebTabs.js
@@ -5,5 +5,18 @@ import { useState } from "react";
 const HOME_URL = "https://www.google.com/webhp?igu=1"
 
 export default function useBearWebTabs() {
-    
+     const [tabs, setTabs] = useState([
+    {
+      id: 1,
+      title: "New Tab",
+      url: "",
+      reloadKey: 0,
+      history: [],
+      historyIndex: -1,
+    },
+  ]);
+
+  const [activeTabId, setActiveTabId] = useState(1);
+  const activeTab = tabs.find((tab) => tab.id === activeTabId) ?? tabs[0];
+
 }
