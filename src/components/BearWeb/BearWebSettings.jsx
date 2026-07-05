df7a4c8471453dab974a1f1cd87a9f6df38bd0ac
Reketino <bjornevensk8@gmail.com>
2026-07-05 09:51:01 +0200

    feat: added new bearweb settings file

---
+++ src/components/BearWeb/BearWebSettings.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function BearWebSettings() {
+    return (
+        <section>
+            <h2>
+                Settings
+            </h2>
+            <p className="text-sm text-neutral-400">
+                Work in progress
+            </p>
+        </section>
+    )
+}
