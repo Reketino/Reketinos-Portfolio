e207ad03148e605afd08662cebd41aeed6ef617a
Reketino <bjornevensk8@gmail.com>
2026-07-08 09:56:08 +0200

    feat: new usesettings file w/ use client, useEffect, useState & const default_settings setup

---
+++ src/components/BearWeb/hooks/useSettings.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const DEFAULT_SETTINGS = {
+    startupNewTab: true,
+    searchEngine: "google",
+    animations: true,
+    glass: true,
+    bearMode: false,
+}
