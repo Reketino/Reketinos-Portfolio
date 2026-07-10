746f0d7043102e84445ab55b241ed15613ccdab3
Reketino <bjornevensk8@gmail.com>
2026-07-10 10:10:18 +0200

    feat: added value & onchange to searchengine

--- src/components/BearWeb/BearWebSettings.jsx
+++ src/components/BearWeb/BearWebSettings.jsx
@@ -73,7 +73,13 @@ export default function BearWebSettings({ onClose, settings, updateSetting }) {
             Search Engine
           </h3>
 
-          <select className="w-full rounded-md border border-neutral-700 bg-neutral-800 px-3 py-2 outline-none">
+          <select 
+          value={settings.searchEngine}
+          onChange={(e) =>
+            updateSetting("searchEngine", e.target.value)
+          }
+          className="w-full rounded-md border border-neutral-700 bg-neutral-800 px-3 py-2 outline-none"
+          >
             <option>Google</option>
             <option>DuckDuckGo</option>
             <option>Bing</option>
