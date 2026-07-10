15ffbfb6d3e15d8e28c0f803fd11a62e07fd4ab9
Reketino <bjornevensk8@gmail.com>
2026-07-10 10:14:41 +0200

    feat: added value to optioons in searchengine

--- src/components/BearWeb/BearWebSettings.jsx
+++ src/components/BearWeb/BearWebSettings.jsx
@@ -80,10 +80,10 @@ export default function BearWebSettings({ onClose, settings, updateSetting }) {
           }
           className="w-full rounded-md border border-neutral-700 bg-neutral-800 px-3 py-2 outline-none"
           >
-            <option>Google</option>
-            <option>DuckDuckGo</option>
-            <option>Bing</option>
-            <option>Brave</option>
+            <option value="google">Google</option>
+            <option value="duckduckgo">DuckDuckGo</option>
+            <option value="bing">Bing</option>
+            <option value="brave">Brave</option>
           </select>
         </div>
 
