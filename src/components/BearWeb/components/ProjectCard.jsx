bb75607de9ad87b08d64f518f2b73dbf0de13c98
Reketino <bjornevensk8@gmail.com>
2026-07-15 10:55:06 +0200

    feat: added button & img logic

--- src/components/BearWeb/components/ProjectCard.jsx
+++ src/components/BearWeb/components/ProjectCard.jsx
@@ -8,6 +8,37 @@ export default function ProjectCard({
     bearMode
 }) {
     return (
-        <button></button>
+        <button
+        onClick={onClick}
+        className={`
+            group
+            overflow-hidden
+            rounded-2xl
+            border
+            text-left
+            transsition-all
+            hover:-translate-y-1
+            hover:shadow-xl
+            ${
+                bearMode
+                ? "border-[#4c6b2c] bg-[#253121] hover:bg-[#31452b]"
+                : "border-neutral-700 bg-neutral-800 hover:bg-neutral-700"
+            }
+            `}
+        >
+            <img
+            src={image}
+            alt={title}
+            className="
+            h-40
+            w-full
+            object-cover
+            transition-transform
+            duration-300
+            group-hover:scale-105
+            "
+            draggable={false}
+            />
+        </button>
     )
 }
