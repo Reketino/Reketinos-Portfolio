05653dbc765c5ade8bef20d975fc325548c6c905
Reketino <bjornevensk8@gmail.com>
2026-07-15 10:47:16 +0200

    feat: added projectcard in new components folder in bearweb w/ export default function in

---
+++ src/components/BearWeb/components/ProjectCard.jsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function ProjectCard({
+    title,
+    description,
+    image,
+    onClick,
+    bearMode
+}) {
+    return (
+        <button></button>
+    )
+}
