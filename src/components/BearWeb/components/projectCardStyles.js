d32c3069c7af1aa25553858f0cc912dece8323f6
Reketino <bjornevensk8@gmail.com>
2026-07-26 10:00:46 +0200

    feat: added const cardanimation

--- src/components/BearWeb/components/projectCardStyles.js
+++ src/components/BearWeb/components/projectCardStyles.js
@@ -6,4 +6,8 @@ export function getProjectCardStyles({ bearMode, animations, glass}) {
   : bearMode
     ?"border-[#4c6b2c] bg-[#253121] hover:bg-[#31452b]"
     : "border-neutral-700 bg-neutral-800 hover:bg-neutral-700"
+
+const cardAnimation = animations
+? "transition-all duration-300 hover:-translate-y-1 hover:shadow-2xl"
+: "";
 }
