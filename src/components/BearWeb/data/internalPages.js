465d88103834be16b3dacdb998dbb588451ce681
Reketino <bjornevensk8@gmail.com>
2026-08-11 00:00:15 +0200

    fix: format internalPages data structure for consistency

--- src/components/BearWeb/data/internalPages.js
+++ src/components/BearWeb/data/internalPages.js
@@ -1,16 +1,16 @@
 export const internalPages = {
-    bearflights: {
-        title: "BearFlights",
-        url: "https://bearflightradar.vercel.app/",
-    },
-    
-    stocks: {
-        title: "BearFlights",
-        url: "https://bearstocks.vercel.app/"
-    },
+  bearflights: {
+    title: "BearFlights",
+    url: "https://bearflightradar.vercel.app/",
+  },
 
-    holdem: {
-        title: "Ber Hold Em",
-        url: "https://bear-hold-em-frontend.vercel.app/"
-    }
-}
+  stocks: {
+    title: "BearFlights",
+    url: "https://bearstocks.vercel.app/",
+  },
+
+  holdem: {
+    title: "Ber Hold Em",
+    url: "https://bear-hold-em-frontend.vercel.app/",
+  },
+};
